b44d6d113e7396eedc19f9a0683b2de7da4c0f57
lakshyajit165 <30868587+lakshyajit165@users.noreply.github.com>
2018-05-16 23:12:37 +0530

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,57 @@
+<?php
+
+include('connection2.php');
+
+$input = $_POST["find"];
+
+$query = "SELECT * FROM users where username = '$input'";
+$result = mysqli_query( $conn, $query );
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container" style="text-align:center;">
+	  
+	  <?php
+
+            if( mysqli_num_rows($result) > 0 ) {
+
+                // we have data!
+                // output the data
+
+                
+
+                while( $row = mysqli_fetch_assoc($result) ) {
+                    echo "<h3>ID:<br></h3>". $row["id"] ."<h3>Name:<br></h3>". $row["username"] ."<h3>Email:<br></h3>". $row["email"] ;
+                }
+
+                
+
+            } else {
+                echo "Whoops! No results.";
+            }
+
+            mysqli_close($conn);
+
+            ?>
+	  
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
